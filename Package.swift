96e5ff608ad243864a171c0f1ca8aa4c88318c50
Mincu Cristian <cristi90mincu@gmail.com>
2020-11-24 10:55:41 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@ import PackageDescription
 
 let package = Package(
     name: "Pixel",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "Pixel",
